abc0670a71e85c7af4158ea3b71850aa503b8bd7
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-02-14 19:10:31 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { GameBoard } from "./components/GameBoaerd";
+import { Player } from "./components/Player";
+
 
 function App() {
-  
-
   return (
-    <p>Coming Soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
