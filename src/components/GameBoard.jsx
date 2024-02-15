5cbab9faddeaf45ff2e7ed4f8fdc4936ff8482ec
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-02-15 18:41:19 +0200

    Update

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,50 +1,23 @@
+export function GameBoard({ onSelectSquare, board }) {
 
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export function GameBoard({ onSelectSquare, turns }) {
-
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns ) {
-
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-
-    // const [gameBoard, setGameBoard]=useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     })
-
-    //     onSelectSquare();
-    // }
-
-
-    return (
-      <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => (
-          <li key={rowIndex}>
-            <ol>
-              {row.map((playerSymbol, colIndex) => (
-                <li key={colIndex}>
-                  <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
-                </li>
-              ))}
-            </ol>
-          </li>
-        ))}
-      </ol>
-    );
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
