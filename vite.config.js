1cf2bca185bcb3d480d4c28e813bb29ab41f72bb
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-02-15 20:35:08 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Tic-Tac-Toe-Game-React/",
+  base:"/tic-tac-toe-project/",
   plugins: [react()],
 })
